7bec1980624de6e9cfe1461ddbebc0402cbd371d
Yousif Mansi <youssef.kamal@btu.edu.eg>
2025-05-13 07:48:39 +0300

    Fix import paths for Zapp compatibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:provider/provider.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+
 import 'auth/firebase_auth/firebase_user_provider.dart';
 import 'auth/firebase_auth/auth_util.dart';
-
 import 'backend/firebase/firebase_config.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 import 'index.dart';
 
